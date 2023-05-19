02f1f3b8d5619c42becf44032610d7c2d1307e88
Enej Simrajh <enej.simrajh@gmail.com>
2023-05-19 13:12:39 +0200

    Initial commit

---
+++ IPSTemplate.UI.Blazor/Base/PageBase.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace IPSDiva.Ui.Blazor;
+
+public abstract class PageBase : ComponentBase
+{
+    [Inject] internal IStringLocalizer<IPSTemplate.UI.Blazor.Properties.Resources> Localizer { get; set; } = default!;
+}
